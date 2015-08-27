6f1319893781dedc0114e5a8dbab39495313b849
Thomas Birke <quafzi@netextreme.de>
2015-08-27 09:44:16 +0200

    Handle conflict with Dhl_Account module

--- src/app/code/community/Quafzi/IgnoreDiscountsForMinimumOrderAmount/Model/Sales/Quote/Address.php
+++ src/app/code/community/Quafzi/IgnoreDiscountsForMinimumOrderAmount/Model/Sales/Quote/Address.php
@@ -8,8 +8,13 @@
  * @author Thomas Birke <tbirke@netextreme.de>
  * @license MIT
  */
+
+if (false === class_exists('Dhl_Account_Model_Quote_Address')) {
+    class Dhl_Account_Model_Quote_Address extends Mage_Sales_Model_Quote_Address {}
+}
+
 class Quafzi_IgnoreDiscountsForMinimumOrderAmount_Model_Sales_Quote_Address
-    extends Mage_Sales_Model_Quote_Address
+    extends Dhl_Account_Model_Quote_Address
 {
     /**
      * Validate minimum amount
